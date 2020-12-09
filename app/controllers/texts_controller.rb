9f6df336a51f6e1da954e7f2ed8bf980b5eb9d6d
kinjo_s <hahaha.kinjo0330@gmail.com>
2020-12-09 09:03:32 +0900

    edit text_controller.rb

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,5 +1,11 @@
 class TextsController < ApplicationController
   def index
+    if index.save
+      redirect_to root_path, notice:  "登録が完了しました。"  
+    else
+      flash[:alert] = "名前を入力してください"
+      render :new
+    end
     
   end
 end

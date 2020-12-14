da6428b68c894815e46230b591a3dcd206f512dd
kinjo_s <hahaha.kinjo0330@gmail.com>
2020-12-14 10:28:02 +0900

    Bootstrap と FontAwesome を インストール

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,11 +1,4 @@
 class TextsController < ApplicationController
   def index
-    if index.save
-      redirect_to root_path, notice:  "登録が完了しました。"  
-    else
-      flash[:alert] = "名前を入力してください"
-      render :new
-    end
-    
   end
 end

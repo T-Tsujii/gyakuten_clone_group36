801aa0ab8998eb7ca0e1b5e8b9e19f552067e0e4
kinjo_s <hahaha.kinjo0330@gmail.com>
2020-12-27 21:38:28 +0900

    create_movies_controller.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,3 +3,7 @@ class Movie < ApplicationRecord
   validates :title, presence: true
   validates :url, presence: true
 end
+
+# require "csv"
+
+# csv_data = CSV.read("movie_data.csv", headers: true)

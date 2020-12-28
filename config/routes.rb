6125c2bfb854c97cfb12ac2a2bb08bc78c20b35c
kinjo_s <hahaha.kinjo0330@gmail.com>
2020-12-28 20:10:32 +0900

    edit_routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'texts#index'
-  resources :texts
+  root to: "texts#index"
+  resources :texts do
+    collection do
+      get "csv_output"
+    end
+  end
 end

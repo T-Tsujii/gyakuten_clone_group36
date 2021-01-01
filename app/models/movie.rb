5880230f8ca3b9d320bc05a08d768effa62f39f5
kinjo_s <hahaha.kinjo0330@gmail.com>
2021-01-01 18:45:59 +0900

    edited_models_movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,15 +3,3 @@ class Movie < ApplicationRecord
   validates :title, presence: true
   validates :url, presence: true
 end
-
-require "csv"
-
-csv_data = CSV.read("movie_data.csv", headers: true)
-puts "start..."
-
-File.open("movie_data.csv", "w") do |file|
-  csv_data.each do |data|
-    puts movie_msg
-    file.write(movie_msg)
-  end
-end

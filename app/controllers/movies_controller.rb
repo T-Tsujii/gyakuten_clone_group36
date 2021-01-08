39fff1e524e108c4b1b95285c39e28225c7b3f6b
kinjo_s <hahaha.kinjo0330@gmail.com>
2021-01-08 20:44:43 +0900

    deleted/movies_controller.rb

--- app/controllers/movies_controller.rb
+++
@@ -1,5 +0,0 @@
-class MoviesController < ApplicationController
-  def index
-    @movies = Movie.id_sorted_only_specific_genre
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "texts#index"
   resources :texts
-  # get "/texts", to: "texts#index"
+  resources :movies
+  # get "/movies(.:format)", to: "movies#index"
 end

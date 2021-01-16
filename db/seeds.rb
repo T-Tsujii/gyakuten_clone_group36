2b1b964e0a5a5caad5ed3137315149c4e4a71eba
Yusaku Kotakemori <yusaku@kotasakuMBP.local>
2021-01-16 22:05:15 +0900

    管理者画面の実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,8 @@ User.find_or_create_by!(email: EMAIL) do |user|
   puts 'ユーザーの初期データインポートに成功しました。'
 end
 
-AdminUser.find_or_create_by!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.find_or_create_by!(email: 'admin@example.com') do |user|
+  user.password = 'password' 
+  user.password_confirmation = 'password'
+  puts 'adminのデータインポートに成功しました。'
+end
